c6834c0431191f662a881a7e1b0107d979f8df0f
Dheeraj-Gehlot <78748838+Dheeraj-Gehlot@users.noreply.github.com>
2021-06-07 11:36:33 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "./Header";
+import App from "./Footer";
+import App from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <Note>
+    <div className="note" >
+      <h1>Javascript and react.js</h1>
+      <p>
+        this was an amazing bootcamp taken up by shaurya sir.we coverd everything from scratch including javascript react,js.
+      </p>
+      </div>
+    </Note>
+  );
+}
+
+export default Note;
